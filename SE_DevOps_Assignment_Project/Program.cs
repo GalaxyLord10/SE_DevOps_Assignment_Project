ee00a249c3bc1aa71111db5e77ce48c3d9f5f278
Keshav <axblze123@gmail.com>
2024-03-03 21:59:42 +0000

    changed connection name for connection string

--- SE_DevOps_Assignment_Project/Program.cs
+++ SE_DevOps_Assignment_Project/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DATABASE_URL"))
 );
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options =>
