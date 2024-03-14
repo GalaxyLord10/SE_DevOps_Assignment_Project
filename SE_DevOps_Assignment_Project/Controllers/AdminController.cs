fb9a04779dccf26294a21855846286b048b78585
Keshav <axblze123@gmail.com>
2024-03-14 21:29:06 +0000

    added in logging to controllers

--- SE_DevOps_Assignment_Project/Controllers/AdminController.cs
+++ SE_DevOps_Assignment_Project/Controllers/AdminController.cs
@@ -6,6 +6,7 @@ using SE_DevOps_DataLayer.Services;
 using System.Security.Claims;
 using SE_DevOps_DataLayer.Common;
 using SE_DevOps_DataLayer.Interfaces;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace SE_DevOps_Assignment_Project.Controllers
 {
@@ -14,41 +15,62 @@ namespace SE_DevOps_Assignment_Project.Controllers
     {
         private readonly ITaskService _taskService;
         private readonly UserManager<IdentityUser> _userManager;
+        private readonly ILogger<AdminController> _logger;
 
-        public AdminController(ITaskService taskService, UserManager<IdentityUser> userManager)
+        public AdminController(ITaskService taskService, UserManager<IdentityUser> userManager, ILogger<AdminController> logger)
         {
             _taskService = taskService;
             _userManager = userManager;
+            _logger = logger;
         }
 
         [Route("/admin/manage-users")]
         public async Task<IActionResult> ManageUsers()
         {
-            var users = _userManager.Users.ToList();
-            
-            return View(users);
+            try
+            {
+                var users = _userManager.Users.ToList();
+
+                return View(users);
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Error getting users list.");
+                throw;
+            }
+
         }
 
         public async Task<IActionResult> AdminDelete(string id)
         {
-            var user = await _userManager.FindByIdAsync(id);
-            if (user != null)
+            try
             {
-                var result = await _userManager.DeleteAsync(user);
-                if (result.Succeeded)
+                var user = await _userManager.FindByIdAsync(id);
+                if (user != null)
                 {
-                    TempData["Message"] = "User deleted successfully!";
-                    TempData["Alert"] = AlertMessages.Success.ToString();
-                    return RedirectToAction("ManageUsers");
+                    var result = await _userManager.DeleteAsync(user);
+                    if (result.Succeeded)
+                    {
+                        TempData["Message"] = "User deleted successfully!";
+                        TempData["Alert"] = AlertMessages.Success.ToString();
+                        return RedirectToAction("ManageUsers");
+                    }
+                    else
+                    {
+                        TempData["Message"] = "User could not be deleted!";
+                        TempData["Alert"] = AlertMessages.Error.ToString();
+                    }
                 }
-                else
-                {
-                    TempData["Message"] = "User could not be deleted!";
-                    TempData["Alert"] = AlertMessages.Error.ToString();
-                }
+
+                return RedirectToAction("ManageUsers");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Error deleting user: {id}");
+                throw;
             }
 
-            return RedirectToAction("ManageUsers");
         }
 
     }
--- SE_DevOps_Assignment_Project/Controllers/TasksController.cs
+++ SE_DevOps_Assignment_Project/Controllers/TasksController.cs
@@ -14,36 +14,44 @@ namespace SE_DevOps_Assignment_Project.Controllers
     {
         private readonly ITaskService _taskService;
         private readonly UserManager<IdentityUser> _userManager;
+        private readonly ILogger<TasksController> _logger;
 
-        public TasksController(ITaskService taskService, UserManager<IdentityUser> userManager)
+        public TasksController(ITaskService taskService, UserManager<IdentityUser> userManager, ILogger<TasksController> logger)
         {
             _taskService = taskService;
             _userManager = userManager;
+            _logger = logger;
         }
 
         public async Task<IActionResult> Index()
         {
-            var user = await GetCurrentUser();
-            var tasks = await _taskService.GetAllTasksAsync(user);
-            TempData["UserName"] = user.UserName;
-            return View(tasks);
-        }
-
-        public async Task<IActionResult> Details(int id)
-        {
-            var user = await GetCurrentUser();
-            var task = await _taskService.GetTaskByIdAsync(id, user);
-            if (task == null)
+            try
             {
-                return NotFound();
+                var user = await GetCurrentUser();
+                var tasks = await _taskService.GetAllTasksAsync(user);
+                TempData["UserName"] = user.UserName;
+                return View(tasks);
             }
-            return View(task);
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error retrieving task list.");
+                throw;
+            }
+
         }
 
         private async Task<IdentityUser> GetCurrentUser()
         {
-            var user = await _userManager.GetUserAsync(User);
-            return user;
+            try
+            {
+                var user = await _userManager.GetUserAsync(User);
+                return user;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error getting current user.");
+                throw;
+            }
         }
 
         public async Task<IActionResult> Create()
@@ -55,10 +63,50 @@ namespace SE_DevOps_Assignment_Project.Controllers
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(TaskViewModel task)
-        {            
-            if (ModelState.IsValid)
+        {
+            try
             {
-                var taskEntity = new SE_DevOps_DataLayer.Entities.Task
+                if (ModelState.IsValid)
+                {
+                    var taskEntity = new SE_DevOps_DataLayer.Entities.Task
+                    {
+                        TaskId = task.TaskId,
+                        Title = task.Title,
+                        Description = task.Description,
+                        DueDate = task.DueDate,
+                        IsCompleted = task.IsCompleted,
+                        Category = task.Category
+                    };
+
+                    var user = await GetCurrentUser();
+                    await _taskService.AddTaskAsync(taskEntity, user);
+                    return RedirectToAction("Index");
+                }
+                return View(task);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error creating task.");
+                throw;
+
+            }
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            try
+            {
+                var user = await GetCurrentUser();
+                var task = await _taskService.GetTaskByIdAsync(id, user);
+
+                if (task == null)
+                {
+                    TempData["Message"] = "Task not found!";
+                    TempData["Alert"] = AlertMessages.Error.ToString();
+                    return NotFound();
+                }
+
+                var taskViewModel = new TaskViewModel()
                 {
                     TaskId = task.TaskId,
                     Title = task.Title,
@@ -68,86 +116,87 @@ namespace SE_DevOps_Assignment_Project.Controllers
                     Category = task.Category
                 };
 
-                var user = await GetCurrentUser();
-                await _taskService.AddTaskAsync(taskEntity, user);
-                return RedirectToAction("Index");
+                return View(taskViewModel);
             }
-            return View(task);
-        }
-
-        public async Task<IActionResult> Edit(int id)
-        {
-            var user = await GetCurrentUser();
-            var task = await _taskService.GetTaskByIdAsync(id, user);
-
-            if (task == null)
+            catch (Exception ex)
             {
-                TempData["Message"] = "Task not found!";
-                TempData["Alert"] = AlertMessages.Error.ToString();
-                return NotFound();
+                _logger.LogError(ex, $"Error getting task {id} for edit.");
+                throw;
             }
-
-            var taskViewModel = new TaskViewModel()
-            {
-                TaskId = task.TaskId,
-                Title = task.Title,
-                Description = task.Description,
-                DueDate = task.DueDate,
-                IsCompleted = task.IsCompleted,
-                Category = task.Category
-            };
-
-            return View(taskViewModel);
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(TaskViewModel task)
         {
-            if (ModelState.IsValid)
+            try
             {
-                var taskEntity = new SE_DevOps_DataLayer.Entities.Task
+                if (ModelState.IsValid)
                 {
-                    TaskId = task.TaskId,
-                    Title = task.Title,
-                    Description = task.Description,
-                    DueDate = task.DueDate,
-                    IsCompleted = task.IsCompleted,
-                    Category = task.Category
-                };
+                    var taskEntity = new SE_DevOps_DataLayer.Entities.Task
+                    {
+                        TaskId = task.TaskId,
+                        Title = task.Title,
+                        Description = task.Description,
+                        DueDate = task.DueDate,
+                        IsCompleted = task.IsCompleted,
+                        Category = task.Category
+                    };
 
-                var user = await GetCurrentUser();
-                await _taskService.UpdateTaskAsync(taskEntity, user);
-                TempData["Message"] = $"Task {task.Title} updated successfully!";
-                TempData["Alert"] = AlertMessages.Success.ToString();
-                return RedirectToAction("Index");
+                    var user = await GetCurrentUser();
+                    await _taskService.UpdateTaskAsync(taskEntity, user);
+                    TempData["Message"] = $"Task {task.Title} updated successfully!";
+                    TempData["Alert"] = AlertMessages.Success.ToString();
+                    return RedirectToAction("Index");
+                }
+
+                return View(task);
             }
-
-            return View(task);
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Error updating task {task.TaskId}.");
+                throw;
+            }
         }
 
         public async Task<IActionResult> Delete(int id)
         {
-            var user = await GetCurrentUser();
-            var task = await _taskService.GetTaskByIdAsync(id, user);
-            if (task == null)
+            try
             {
-                TempData["Message"] = "Task could not be deleted";
-                TempData["Alert"] = AlertMessages.Error.ToString();
-                return NotFound();
+                var user = await GetCurrentUser();
+                var task = await _taskService.GetTaskByIdAsync(id, user);
+                if (task == null)
+                {
+                    TempData["Message"] = "Task could not be deleted";
+                    TempData["Alert"] = AlertMessages.Error.ToString();
+                    return NotFound();
+                }
+                return View(task);
             }
-            return View(task);
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Error getting task {id} for deletion.");
+                throw;
+            }
         }
 
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            var user = await GetCurrentUser();
-            await _taskService.DeleteTaskAsync(id, user);
-            TempData["Message"] = "Task deleted!";
-            TempData["Alert"] = AlertMessages.Success.ToString();
-            return RedirectToAction("Index");
+            try
+            {
+                var user = await GetCurrentUser();
+                await _taskService.DeleteTaskAsync(id, user);
+                TempData["Message"] = "Task deleted!";
+                TempData["Alert"] = AlertMessages.Success.ToString();
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Error deleting task {id}.");
+                throw;
+            }
         }
     }
 }
